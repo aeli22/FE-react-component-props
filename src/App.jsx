f13a7e96794315ec9524966ef93ba3b74ed311be
aeli kim <aelli2002@naver.com>
2024-05-06 16:40:32 +0900

    4번 수정

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
             }}
         >
             <BoxAlign />
-            {/* <SelectButtons /> */}
-            {/* <PlayGround /> */}
+            {<SelectButtons />}
+            {<PlayGround />}
         </div>
     );
 }
